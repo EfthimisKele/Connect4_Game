a68333c6818d2c35367b19bcb1de57e40102be03
EfthimisKele <m.efthimis4@gmail.com>
2020-10-31 15:55:18 +0200

    Create px.php
    just practice , to see if it works

---
+++ px.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Askisi</title>
+</head>
+<body>
+    <h1>
+        YOOOOOOOLOOOO <br>
+        <?php
+            $a =9;
+            echo --$a.'<br>'; 
+            echo $a.'<br>';
+            $channel = "Tech and Code";
+            $name = "Bill";
+            $name1 = "     Bill     ";
+
+            echo str_word_count($channel) . '<br/>' . $name . '<br/>'; 
+            echo strlen($channel) . '<br/>';
+            echo trim($name1);
+
+
+        ?>
+    </h1>
+    
+</body>
+</html>

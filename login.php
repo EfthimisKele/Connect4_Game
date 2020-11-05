91b06c1dcc69cb2440b833653ada6ad829ad960e
EfthimisKele <m.efthimis4@gmail.com>
2020-11-05 17:24:05 +0200

    Create files
    create some files

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Σύνδεση</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <div class="">
+        </div>
+        <div class="form-container">
+            <h2>Σύνδεση</h2>
+            <form method="post">
+                <input type="text" name="username" placeholder="Όνομα Χρήστη">
+                <input type="password" name="password" placeholder="Κωδικός">
+                <input type="submit" name="login" value="Σύνδεση">
+            </form>
+        </div>
+    </div>
+</body>
+</html>

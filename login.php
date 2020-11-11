453f51e5bfe480f176b200b6a78db00e95995299
EfthimisKele <m.efthimis4@gmail.com>
2020-11-11 23:45:09 +0200

    create login.php and login.css
    create login.php and login.css

--- login.php
+++ login.php
@@ -8,17 +8,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Σύνδεση</title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
     <div class="container">
         <div class="">
         </div>
         <div class="form-container">
+            <h1>ΚΑΛΩΣ ΟΡΙΣΑΤΕ ΣΤΟ ΠΑΙΧΝΙΔΙ ΣΚΟΡ4 !!</h1>
             <h2>Σύνδεση</h2>
             <form method="post">
                 <input type="text" name="username" placeholder="Όνομα Χρήστη">
                 <input type="password" name="password" placeholder="Κωδικός">
-                <input type="submit" name="login" value="Σύνδεση">
+                <input type="submit" name="register" value="Εγγραφή"/> 
+                <input type="submit" name="login" value="Σύνδεση"/>                
             </form>
         </div>
     </div>

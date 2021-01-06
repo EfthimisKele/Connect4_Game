4059b674eaeed97e0f4bdf3158b770939beaf8bd
EfthimisKele <m.efthimis4@gmail.com>
2021-01-06 13:38:19 +0200

    finish summary

--- js/create.js
+++ js/create.js
@@ -21,7 +21,7 @@ function draw_board() {
     for(var i=1;i<7;i++) {
         t +='<tr>';
         for(var j=1;j<8;j++){
-            t += '<td class="score_circle" id="circle_' + i+ '_' + j + '">' + i + ',' + j + '</td>';
+            t += '<td class="score_circle" id="circle_' + i + '_' + j + '">' + i + ',' + j + '</td>';
         }
         t+='</tr>';
     }
--- lib/config_local.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-	$DB_PASS = 'Al!er1919';
-	$DB_USER = 'root';
-?>
